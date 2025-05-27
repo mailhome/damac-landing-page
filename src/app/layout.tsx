4ab3db2597eb41c1df06b9f6f98d001e48d169ed
Henry Abraham <mailhome.henry@gmail.com>
2025-05-27 02:51:54 +0100

    First Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {  Anek_Latin } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const aneklatin = Anek_Latin({
+  subsets: ["latin"]
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${aneklatin.className}  antialiased`}
       >
         {children}
       </body>
